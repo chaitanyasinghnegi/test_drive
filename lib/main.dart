40c18e0ecb2f091aa33f9041e9784bec7989e1fb
Chaitanya Negi <chaitanyanegi1@gmail.com>
2024-01-01 23:39:20 +0530

    text-button added, firebase auth tried-out. input and register email-password.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(
@@ -13,20 +14,63 @@ void main() {
   );
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Hello'),
+        backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: TextButton(
-          onPressed: () async {},
-          child: const Text('Register'),
-        ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            decoration: const InputDecoration(
+              hintText: 'Enter your email here',
+            ),
+          ),
+          TextField(
+            controller: _password,
+            decoration: const InputDecoration(
+              hintText: 'Enter your password here',
+            ),
+          ),
+          TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final password = _password.text;
+              await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                email: email, 
+                password: password);
+            },
+            child: const Text('Register'),
+          ),
+        ],
       ),
     );
   }
